0bc3b9523e3c4c73da7eb951d8f9e61ed2b48ecf
Elvin Efendi <elvin.efendiyev@gmail.com>
2026-01-02 13:22:46 -0500

    Skip SMTP auth when credentials missing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,17 +8,18 @@ Rails.application.configure do
   # SMTP setting can be configured via environment variables.
   # For other configuration options, consult the Action Mailer documentation.
   if smtp_address = ENV["SMTP_ADDRESS"].presence
+    smtp_user = ENV["SMTP_USERNAME"].presence
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
       address: smtp_address,
       port: ENV.fetch("SMTP_PORT", ENV["SMTP_TLS"] == "true" ? "465" : "587").to_i,
       domain: ENV.fetch("SMTP_DOMAIN", nil),
-      user_name: ENV.fetch("SMTP_USERNAME", nil),
-      password: ENV.fetch("SMTP_PASSWORD", nil),
-      authentication: ENV.fetch("SMTP_AUTHENTICATION", "plain"),
+      user_name: smtp_user,
+      password: smtp_user ? ENV.fetch("SMTP_PASSWORD", nil) : nil,
+      authentication: smtp_user ? ENV.fetch("SMTP_AUTHENTICATION", "plain") : nil,
       tls: ENV["SMTP_TLS"] == "true",
       openssl_verify_mode: ENV["SMTP_SSL_VERIFY_MODE"]
-    }
+    }.compact
   end
 
   # Base URL for links in emails and other external references.
